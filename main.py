0df86a18610c125f6fb4566c1c1558c79f5b23d4
YotaBitOn <artemyasinets@gmail.com>
2026-02-02 16:55:44 +0200

    first commit, part2.Now really added DB and UI

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import sqlite3
+import sys
+
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
 
 print('Hello, World!')
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('user_data.db')
 cursor = conn.cursor()
+
+loader = QUiLoader()
+
+app = QApplication(sys.argv)
+mw = loader.load('main_v19.ui', None)
+
+mw.show()
+app.exec()
